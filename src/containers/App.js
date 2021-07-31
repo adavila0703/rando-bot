8c0ee32815b6acd68e4ebc45690167a0b875f92f
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2021-07-31 06:36:39 -0500

    Converted app.js to use hooks, added error boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,34 @@
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Refresh from "../components/Refresh";
+import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: "",
-        };
-    }
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((users) => this.setState({ robots: this.randoDrop(users) }));
-    }
+            .then(response => response.json())
+            .then(users => setRobots(randoDrop(users)));
+    }, []);
 
-    randoDrop(users) {
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    };
+
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
+    const filterRobots = robots.filter((robot) => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    const randoDrop = (users) => {
         let percentage = 0.5;
         let randomNum = Math.random();
         users.forEach((element) => {
@@ -30,32 +38,20 @@ class App extends Component {
         return users.filter((element) => element.dropRate < randomNum);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    };
-
-    reloadState = () => {
-        window.location.reload();
-    };
-
-    render() {
-        const { robots, searchfield } = this.state;
-        const filterRobots = robots.filter((robot) => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-        return !robots.length ? (
-            <h1>Loading...</h1>
-        ) : (
-            <div className="tc">
-                <h1 className="f2">rando-bot</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Refresh refresh={this.reloadState} />
-                <Scroll>
+    return !robots.length ? (
+        <h1>Loading...</h1>
+    ) : (
+        <div className="tc">
+            <h1 className="f2">rando-bot</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <Refresh refresh={reloadPage} />
+            <Scroll>
+                <ErrorBoundary>
                     <CardList robots={filterRobots} />
-                </Scroll>
-            </div>
-        );
-    }
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
